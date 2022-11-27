05573020b59905c14a6fec7be537248815ffa161
amIworking <slavaa2378@gmail.com>
2022-11-27 18:17:05 +0300

    RememberApp v3.4.2
    - Making a progress bar during training
    - Adding a btn 'I was right' which makes the result becoming true
    - Adding a feature to switch between words and their translations

--- static/main_page/js/repeating.js
+++ static/main_page/js/repeating.js
@@ -1,14 +1,37 @@
 let target_dict = document.getElementById('target_dict').innerHTML;
 res = target_dict.replace(/"/g, '\\\"').replace(/\'/g, '"');
-const mydata = JSON.parse(res);
+let mydata = JSON.parse(res);
+const COUNT = 3;
 console.log(mydata);
 let test = {}
 for (const[key,value] of Object.entries(mydata)){
     test[key] = 0;
 }
+AMOUNT_COUNT = Object.keys(test).length*COUNT;
 let random_word;
+let correct_flag = document.getElementById('correct_flag');
+let secion_count = 0;
+
+const PROGRESS = document.querySelector('progress')
+
+PROGRESS.setAttribute('value', 22);
+
+
+function swithcing_words(){
+    let newData= {};
+    let newTest= {};
+    for (const[key,value] of Object.entries(mydata)){
+    newTest[value] = 0;
+    newData[value] = key;
+    }
+    mydata = newData;
+    test = newTest;
+    console.log(mydata,test);
+    randomizer();
+}
 
 function randomizer(){
+    PROGRESS.setAttribute('value', (secion_count/AMOUNT_COUNT)*100);
     insert_word = document.getElementById('question_word')
     let test_keys = Object.keys(test)
     if (test_keys.length>0){
@@ -18,26 +41,46 @@ function randomizer(){
     }
 }
 
+function correcting(){
+    secion_count++;
+    test[question_word]++;
+    ans_res.style.color = 'green';
+    ans_res.innerHTML = `Right!\nYou answered right. ${1+COUNT-test[question_word]} out of ${COUNT} times left`;
+    if (test[question_word]==COUNT){
+        delete test[question_word];
+        }
+    correct_flag.style.display='none';
+    randomizer();
+}
 
 function check1(){
-    if (Object.keys(test).length>0){
-        let answer = document.getElementById('ans').value;
-        document.getElementById('ans').value=''
-        question_word = document.getElementById('question_word').innerHTML;
-        ans_res = document.getElementById('ans_res');
-        if (mydata[question_word]==answer){
-            test[question_word]++;
-            ans_res.style.color = 'green';
-            ans_res.innerHTML = `Right!\nYou answered right. ${3-test[question_word]} out of 3 times left`;
-            if (test[question_word]==3){
+    if (Object.keys(test).length<=0) {
+
+        alert('finished');
+        let dict_name = document.getElementById('dict_name').innerHTML;
+        location.href = `/adding_points/${dict_name}`;
+    }
+    let answer = document.getElementById('ans').value;
+    document.getElementById('ans').value='';
+    question_word = document.getElementById('question_word').innerHTML;
+    ans_res = document.getElementById('ans_res');
+    if (mydata[question_word]==answer){
+        correct_flag.style.display='none';
+        test[question_word]++;
+        secion_count++;
+        ans_res.style.color = 'green';
+        ans_res.innerHTML = `Right!\nYou answered right. ${1+COUNT-test[question_word]} out of ${COUNT} times left`;
+        if (test[question_word]==COUNT){
             delete test[question_word];
             } 
         }
-        else {
-            ans_res.style.color = 'red';
-            ans_res.innerHTML = `Wrong!\nThe right answer is "${mydata[question_word]}"`;
-    }
-    }
+    else {
+        ans_res.style.color = 'red';
+        ans_res.innerHTML = `Wrong!\nThe right answer is "${mydata[question_word]}"`;
+        correct_flag.style.display='inline';
+        }    
+
+
     if (Object.keys(test).length<=0) {
         alert('finished');
         let dict_name = document.getElementById('dict_name').innerHTML;
@@ -46,6 +89,29 @@ function check1(){
     randomizer()
     console.log(test)
     
+}
+
+function check_answer(){
+    let answer = document.getElementById('ans').value;
+    question_word = document.getElementById('question_word').innerHTML;
+    if (mydata[question_word]==answer){
+            test[question_word]++;
+            secion_count++;
+            ans_res.style.color = 'green';
+            ans_res.innerHTML = `Right!\nYou answered right. ${1+COUNT-test[question_word]} out of 3 times left`;
+            if (test[question_word]==3){
+            delete test[question_word];
+            }
+            document.getElementById('ans').value='';
+            console.log(test)
+            randomizer();
+    }
+    if (Object.keys(test).length<=0) {
+        alert('finished');
+        let dict_name = document.getElementById('dict_name').innerHTML;
+        location.href = `/adding_points/${dict_name}`;
+    }
+
 }
 
 function enter_cheсk(e){
